dce7010809e6dd7dbe642e3886d69984271fba55
Can Baran Engin <canbaranengin1@gmail.com>
2022-04-25 15:36:13 +0300

    Kdv Hesaplama odevi

---
+++ Java_101/KdvHesaplama/KdvHesaplama.java
@@ -0,0 +1,33 @@
+package Java_101.KdvHesaplama;
+import java.util.Scanner;
+
+public class KdvHesaplama {    
+    public static void main(String[] args) {
+        double Tutar;
+        float kdv=0.18f;
+        double kdvu=0.8;
+
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Tutar Giriniz:");
+        Tutar = input.nextDouble();
+
+        double kdvtut = (Tutar<1000) ? Tutar*kdv : Tutar*kdvu;
+
+        System.out.print("Kdv'li tutar: ");
+        System.out.println(Tutar+kdvtut);
+        System.out.println("Kdv miktarı:"+kdvtut);
+
+    }
+
+
+
+
+
+        
+
+
+
+    
+    
+}
