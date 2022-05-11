2ebc53aa3a3a6b8840c635dbdeaa0f14a6308dd9
Can Baran Engin <canbaranengin1@gmail.com>
2022-05-11 09:10:33 +0300

    fibonnaci series created

---
+++ Java_101/FibonacciSeries/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+
+    static int fibonnacciSum(int n){
+
+        if (n==1 || n==2) {
+            return 1;
+        }
+
+        int sum=fibonnacciSum(n-1) + fibonnacciSum(n-2);
+
+        return sum;
+
+    }
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(fibonnacciSum(6));
+
+        
+        
+    }
+}
